4c5668e3ee8d16e71424b23844c91e2858688a77
Derek Hogue <derek@amphibian.info>
2016-08-24 07:56:56 -0500

    Fix stray typo

--- ee3/user/addons/classee_body/pi.classee_body.php
+++ ee3/user/addons/classee_body/pi.classee_body.php
@@ -1,4 +1,4 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');s
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
     This file is part of Classee Body add-on for ExpressionEngine.
